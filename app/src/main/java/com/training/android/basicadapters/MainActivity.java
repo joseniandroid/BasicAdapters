2afab3c224708d6d022081eceda12a4104bbe922
Ahdzlee Formentera <dev.ahdzlee@gmail.com>
2016-08-30 01:09:12 +0800

    Add main topics to MainActivity.

--- app/src/main/java/com/training/android/basicadapters/MainActivity.java
+++ app/src/main/java/com/training/android/basicadapters/MainActivity.java
@@ -1,13 +1,25 @@
 package com.training.android.basicadapters;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String[] mTopics = {
+            "Simple Adapter",
+            "ListView with Custom Adapter"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listView = (ListView) findViewById(R.id.listView);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, mTopics);
+
+        listView.setAdapter(adapter);
     }
 }
